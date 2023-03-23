0aa6d9af08381c257e4bd338f99d30388cde4a4f
sekarmano14 <sekarmano46@gmail.com>
2023-03-23 20:00:50 +0530

    Project structure

---
+++ src/main/java/org/pageobject/LoginPage.java
@@ -0,0 +1,12 @@
+package org.pageobject;
+
+import org.base.BaseClass;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BaseClass {
+
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+}
