47ce1530a9f7861aac7689af2409a7dcb935147a
sekarmano14 <sekarmano46@gmail.com>
2023-03-27 19:44:45 +0530

    Kumar's Code

--- src/test/java/org/test/TestClass.java
+++ src/test/java/org/test/TestClass.java
@@ -2,4 +2,10 @@ package org.test;
 
 public class TestClass {
 
+	public static void main(String[] args) {
+		System.out.println("Test1");
+		System.out.println("Test4");
+		System.out.println("Test5");
+	}
+
 }
